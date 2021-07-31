730f77ca6bae9d051f21dac4d23228833b7621a0
Aza Maulana Hadi <azamaulanaaa@gmail.com>
2021-07-31 10:59:04 +0700

    fix(nvimtree): reconfig and clean up

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,15 +1,12 @@
-vim.g.nvim_tree_side = 'right'
-vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_auto_ignore_ft = {}
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_root_folder_modifier = ':~'
-vim.g.nvim_tree_auto_resize = 0
-vim.g.nvim_tree_window_picker_exclude = {
+local g = vim.g
+
+-- Window
+g.nvim_tree_side = 'left'
+g.nvim_tree_width = 30
+g.nvim_tree_auto_close = 1
+g.nvim_tree_auto_resize = 0
+g.nvim_tree_follow = 1
+g.nvim_tree_window_picker_exclude = {
 	filetype = {
 		'packer',
 	},
@@ -18,7 +15,22 @@ vim.g.nvim_tree_window_picker_exclude = {
 	},
 }
 
+-- File list
+g.nvim_tree_hide_dotfiles = 1
+
+-- Look
+g.nvim_tree_root_folder_modifier = table.concat {":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"}
+g.nvim_tree_add_trailing = 0
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1
+}
+g.nvim_tree_icons = {
+    default = "",
+}
+
+-- Keybinding
 vim.api.nvim_set_keymap('n', '<c-o>', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
-vim.cmd([[
-	highlight NvimTreeFolderIcon guibg=blue
-]])
