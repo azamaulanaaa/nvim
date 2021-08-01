6108321b309090993cedc61b9b849c30fc4e1327
Aza Maulana Hadi <azamaulanaaa@gmail.com>
2021-08-01 09:53:28 +0700

    fix(telescope): performance issue with node_modules
    Ignoring file from node_modules and .git seem the best solusion for
    right now.

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -5,6 +5,15 @@ return {
 		{ 'nvim-lua/plenary.nvim' },
 	},
 	config = function()
+		require('telescope').setup{
+			defaults = {
+				file_ignore_patterns = {
+					'.git',
+					'node_modules',
+				},
+			},
+		}
+
 		vim.api.nvim_set_keymap('n', '<leader>f', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], { noremap = true, silent = true } )
 	end,
 }
