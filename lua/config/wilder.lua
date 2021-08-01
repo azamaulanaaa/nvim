24d4c3b95f8f489b216fae4bb39795e969649947
Aza Maulana Hadi <azamaulanaaa@gmail.com>
2021-08-01 22:51:48 +0700

    refactor(plugins): call vim function as lua function

--- lua/config/wilder.lua
+++ lua/config/wilder.lua
@@ -1,9 +1,7 @@
 return {
 	'gelguy/wilder.nvim',
 	config = function()
-		vim.cmd([[
-			call wilder#enable_cmdline_enter()
-		]])
+		vim.fn['wilder#enable_cmdline_enter']()
 
 		-- Show sugestion
 		vim.o.wildcharm = 9 -- 9 for <Tab>
@@ -13,8 +11,6 @@ return {
 		vim.api.nvim_set_keymap('c', '<s-tab>', [[wilder#in_context() ? wilder#previous() : '<s-tab>']], { expr = true })
 
 		-- Trigger
-		vim.cmd([[
-			call wilder#set_option('modes', ['/', '?', ':'])
-		]])
+		vim.fn['wilder#set_option']('modes', {'/', '?', ':'})
 	end,
 }
