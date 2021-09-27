09248417c0c159c83975ba8db5882171e5479951
Aza Maulana Hadi <azamaulanaaa@gmail.com>
2021-09-27 18:40:42 +0700

    fix(nvimtree): update to new config format

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -9,19 +9,6 @@ return {
     },
     setup = function()
         local config = {
-            nvim_tree_side = 'left',
-            nvim_tree_width = 30,
-            nvim_tree_auto_close = 1,
-            nvim_tree_auto_resize = 0,
-            nvim_tree_follow = 1,
-            nvim_tree_window_picker_exclude = {
-                filetype = {
-                    'packer',
-                },
-                buftype = {
-                    'terminal',
-                },
-            },
             nvim_tree_hide_dotfiles = 1,
             nvim_tree_root_folder_modifier = table.concat {':t:gs?$?/..', string.rep(' ', 1000), '?:gs?^??'},
             nvim_tree_add_trailing = 0,
@@ -38,8 +25,16 @@ return {
         for name, value in pairs(config) do
             vim.g[name] = value
         end
+
     end,
     config = function()
+        require('nvim-tree').setup {
+           auto_close = true,
+           update_focused_file = {
+               enable = true,
+           },
+        }
+
         -- Keybinding
         vim.api.nvim_set_keymap('n', '<c-o>', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent = true })
     end,
