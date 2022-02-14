6bf92b873e6f88ba50cafb6d5a396fbbd96a94de
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-14 14:10:54 +0700

    feat: add neovim setup over ppa

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -2,5 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s $SCRIPT_DIR/config ~/.config/nvim
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim
+
 ln -s $SCRIPT_DIR/config/ ~/.config/nvim/
