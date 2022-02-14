36b4713eeb965027e832e964953b4f1a38fe99fd
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-14 15:45:31 +0700

    feat: add nvim formater for golang

--- neovim/config/lua/config/formatter.lua
+++ neovim/config/lua/config/formatter.lua
@@ -3,6 +3,7 @@ return {
     opt = true,
     ft = {
         'rust',
+        'go',
     },
     config = function()
         require("formatter").setup({
@@ -15,6 +16,15 @@ return {
                             stdin = true,
                         }
                     end,
+                },
+                go = {
+                    function()
+                        return {
+                            exe = "gofmt",
+                            args = {vim.api.nvim_buf_get_name(0)},
+                            stdin = true,
+                        }
+                    end
                 },
             },
         })
