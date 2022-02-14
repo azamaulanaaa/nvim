e5159ebdaa6dda15c8322e8910f61dbd78c87d98
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-14 14:44:31 +0700

    feat: add dnscrypt-proxy

---
+++ dnscrypt-proxy/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install \
+    resolvconf \
+    dnscrypt-proxy
+
+# sudo echo "nameserver 127.0.2.1" >> /etc/resolv.conf
+# sudo echo "nameserver 1.1.1.1" >> /etc/resolv.conf
