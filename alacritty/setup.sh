38a3123365e3875a5e4e4f7ce844f67203a9affc
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-14 14:39:34 +0700

    fix: require press y for installation

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -2,4 +2,4 @@
 
 sudo add-apt-repository -y ppa:aslatter/ppa
 sudo apt update
-sudo apt install alacritty
+sudo apt install -y alacritty
--- neovim/setup.sh
+++ neovim/setup.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
-sudo apt install neovim
+sudo apt install -y neovim
 
 ln -s $SCRIPT_DIR/config/ ~/.config/nvim/
