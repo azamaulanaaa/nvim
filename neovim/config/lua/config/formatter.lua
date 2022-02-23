86aa92582e302b27aa83b20bba2287e9ff65c73a
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-23 21:54:50 +0700

    feat(neovim): add formatter for javascript family
    - javascript (js)
    - javascriptreact (jsx)
    - typescript (ts)
    - typescriptreact (tsx)

--- neovim/config/lua/config/formatter.lua
+++ neovim/config/lua/config/formatter.lua
@@ -4,6 +4,10 @@ return {
     ft = {
         'rust',
         'go',
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
     },
     config = function()
         require("formatter").setup({
@@ -26,12 +30,64 @@ return {
                         }
                     end,
                 },
+                javascript = {
+                    function()
+                        return {
+                            exe = "prettier",
+                            args = {
+                                "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                '--single-quote',
+                                '--tab-width', 4
+                            },
+                            stdin = true,
+                        }
+                    end
+                },
+                javascriptreact = {
+                    function()
+                        return {
+                            exe = "prettier",
+                            args = {
+                                "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                '--single-quote',
+                                '--tab-width', 4
+                            },
+                            stdin = true,
+                        }
+                    end
+                },
+                typescript = {
+                    function()
+                        return {
+                            exe = "prettier",
+                            args = {
+                                "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                '--single-quote',
+                                '--tab-width', 4
+                            },
+                            stdin = true,
+                        }
+                    end
+                },
+                typescriptreact = {
+                    function()
+                        return {
+                            exe = "prettier",
+                            args = {
+                                "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                                '--single-quote',
+                                '--tab-width', 4
+                            },
+                            stdin = true,
+                        }
+                    end
+                },
             },
         })
         vim.api.nvim_exec([[
             augroup FormatAutogroup
                 autocmd!
-                autocmd BufWritePost *.rs,*.go FormatWrite
+                autocmd BufWritePost *.rs,*.go,*.js,*.jsx,*.ts,*.tsx FormatWrite
             augroup END
         ]], true)
     end,
