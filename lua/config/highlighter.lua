8e34d8db0d232ea294060543209ebfe698ef23aa
Aza Maulana <azamaulanaaa@gmail.com>
2023-01-03 22:54:58 +0700

    fix(highlighter): make it non optional

--- lua/config/highlighter.lua
+++ lua/config/highlighter.lua
@@ -1,41 +1,8 @@
 return {
     'nvim-treesitter/nvim-treesitter', 
     branch = 'master',
-    run = ':TSUpdate',
-    opt = true,
-    ft = {
-        'bash',
-        'dockerfile',
-        'go',
-        'html',
-        'javascript',
-        'javascriptreact',
-        'json',
-        'lua',
-        'python',
-        'typescript',
-        'typescriptreact',
-        'yaml',
-    },
+    --run = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup {
-            ensure_installed = {
-                'bash',
-                'dockerfile',
-                'go',
-                'html',
-                'javascript',
-                'json',
-                'lua',
-                'python',
-                'tsx',
-                'typescript',
-                'yaml',
-            },
-            hightlight = {
-                enable = true,
-                disable = {},
-            },
-        }
+        require('nvim-treesitter.configs').setup{}
     end,
 }
