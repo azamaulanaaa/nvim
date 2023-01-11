31ddd383deea2a64f8ee994bb278e9f65798b4d3
Aza Maulana <azamaulanaaa@gmail.com>
2023-01-11 13:15:57 +0700

    fix(terminal): for window use powershell instead

--- lua/setting.lua
+++ lua/setting.lua
@@ -27,6 +27,12 @@ function setting()
         sidescrolloff   = 12,
         wrap            = false,
         guifont         = 'FiraCode Nerd Font:h10',
+        shell           = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+        shellcmdflag    = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+        shellredir      = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+        shellpipe       = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+        shellquote      = "",
+        shellxquote     = "",
     }
 
     for name, value in pairs(setup) do
