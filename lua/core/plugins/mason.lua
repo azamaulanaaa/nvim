fe406e4e39b6cda65d80ab53557c7eb133f99ba6
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 10:54:41 +0700

    chore(core): mason does not have ensure installed config

--- lua/core/plugins/mason.lua
+++ lua/core/plugins/mason.lua
@@ -2,10 +2,4 @@ local M = { "mason-org/mason.nvim" }
 
 M.cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" }
 
-M.opts = function()
-	return {
-		ensure_installed = { "lua-language-server" },
-	}
-end
-
 return M
