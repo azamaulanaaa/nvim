edbca3d2fa6e3f1c15c115c93cf51bb2ad9c8d60
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 17:39:03 +0700

    fix(core): nvim tree resizeable does not work

--- lua/core/plugins/nvimtree.lua
+++ lua/core/plugins/nvimtree.lua
@@ -48,25 +48,4 @@ M.opts = function(_, opts)
 	return opts
 end
 
-M.config = function(_, opts)
-	require("nvim-tree").setup(opts)
-
-	local api = require("nvim-tree.api")
-
-	vim.api.nvim_create_augroup("NvimTreeResize", {
-		clear = true,
-	})
-
-	vim.api.nvim_create_autocmd({ "VimResized", "WinResized" }, {
-		group = "NvimTreeResize",
-		callback = function()
-			-- Get the nvim-tree window ID
-			local winid = api.tree.winid()
-			if winid then
-				api.tree.reload()
-			end
-		end,
-	})
-end
-
 return M
