3628a74d86565b27dc65ecba553850260446f785
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 13:55:28 +0700

    chore(core): re-format conform

--- lua/core/plugins/conform.lua
+++ lua/core/plugins/conform.lua
@@ -1,24 +1,24 @@
 local M = { "stevearc/conform.nvim" }
 
 M.event = function(_, _events)
-	return { "BufWritePre" }
+  return { "BufWritePre" }
 end
 
 M.cmd = function(_, cmds)
-	return { "ConformInfo" }
+  return { "ConformInfo" }
 end
 
 M.opts = function(_, opts)
-	opts.formatters_by_ft = opts.formatters_by_ft or {}
+  opts.formatters_by_ft = opts.formatters_by_ft or {}
 
-	opts.lua = opts.lua or {}
-	opts.lua = vim.tbl_deep_extend("force", { "stylua" }, opts.lua)
+  opts.lua = opts.lua or {}
+  table.insert(opts.lua, "stylua")
 
-	opts.format_on_save = opts.format_on_save or {}
-	opts.format_on_save.timeout_ms = 3000
-	opts.format_on_save.lsp_format = "fallback"
+  opts.format_on_save = opts.format_on_save or {}
+  opts.format_on_save.timeout_ms = 3000
+  opts.format_on_save.lsp_format = "fallback"
 
-	return opts
+  return opts
 end
 
 return M
