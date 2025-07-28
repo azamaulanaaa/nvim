6c299ef5d15e2022634f50cd34155d91861b6452
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 16:22:39 +0700

    feat(core): add leap

---
+++ lua/core/plugins/leap.lua
@@ -0,0 +1,12 @@
+local M = {
+  "ggandor/leap.nvim",
+  dependencies = {
+    "tpope/vim-repeat"
+  }
+}
+
+M.config = function(_, configs)
+  require("leap").add_default_mappings()
+end
+
+return M
