882c2255ce9d0d783d13b1a233b73b95a422328a
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 18:56:52 +0700

    feat(custom): add colorschema
    use carbon fox colorschema

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+local M = {
+	"EdenEast/nightfox.nvim",
+}
+
+M.opts = function(_, opts)
+	return opts
+end
+
+M.config = function(_, opts)
+	require("nightfox").setup(opts)
+
+	vim.opt.background = "dark"
+	vim.cmd.colorscheme("carbonfox")
+end
+
+return M
