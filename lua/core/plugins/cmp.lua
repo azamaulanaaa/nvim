fde782216bea5712898769ec6d5ffbf6a6ab7b96
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 16:45:40 +0700

    feat(core): add keybinding for cmp

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -16,6 +16,8 @@ M.event = function(_, events)
 end
 
 M.opts = function(_, opts)
+	local cmp = require("cmp")
+
 	opts.completion = opts.completion or {}
 	opts.completion.completeopt = "menu,menuone"
 
@@ -29,6 +31,30 @@ M.opts = function(_, opts)
 	table.insert(opts.sources, { name = "buffer" })
 	table.insert(opts.sources, { name = "path" })
 	table.insert(opts.sources, { name = "luasnip" })
+
+	opts.mapping = opts.mapping or {}
+	opts.mapping["<CR>"] = cmp.mapping.confirm({
+		behavior = cmp.ConfirmBehavior.Insert,
+		select = true,
+	})
+	opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		elseif require("luasnip").expand_or_jumpable() then
+			require("luasnip").expand_or_jump()
+		else
+			fallback()
+		end
+	end, { "i", "s" })
+	opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif require("luasnip").jumpable(-1) then
+			require("luasnip").jump(-1)
+		else
+			fallback()
+		end
+	end, { "i", "s" })
 
 	return opts
 end
