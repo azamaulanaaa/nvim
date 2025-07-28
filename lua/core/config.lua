9b50719326be348c4d7136585584b731817ff110
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 10:03:22 +0700

    feat(core): add base vim config
    - fileformat using unix
    - indentation using 2 spaces

---
+++ lua/core/config.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+-- file format
+opt.fileformats = "unix"
+
+-- indentation
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
