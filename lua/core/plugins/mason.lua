9d15f7a3cb29588380ba74650323d5f5190f1c5d
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-28 10:44:15 +0700

    fix(core): mason has wrong config setup

--- lua/core/plugins/mason.lua
+++ lua/core/plugins/mason.lua
@@ -3,13 +3,9 @@ local M = {"williamboman/mason.nvim"}
 M.cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" }
 
 M.opts = function()
-  return {
-    ensure_installed = { "lua-language-server" },
-  }
-end
-
-M.config = function(_, opts)
-  require("mason").setup(opts)
+	return {
+		ensure_installed = { "lua-language-server" },
+	}
 end
 
 return M
