c22b61ae65129a7bf8d2e0f8ae143aeb5d3ab01f
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-01 11:36:13 +0700

    feat(core): replace nvim cmp with blink cmp

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -1,62 +1,28 @@
 local M = {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"L3MON4D3/LuaSnip",
-		{ "windwp/nvim-autopairs", config = true },
-	},
+  "saghen/blink.cmp",
+  version = "1.*",
+  dependencies = {
+    { "windwp/nvim-autopairs", config = true },
+  },
 }
 
-M.event = function(_, events)
-	table.insert(events, "InsertEnter")
-
-	return events
-end
-
 M.opts = function(_, opts)
-	local cmp = require("cmp")
+  opts.completion = opts.completion or {}
 
-	opts.completion = opts.completion or {}
-	opts.completion.completeopt = "menu,menuone"
+  opts.completion.documentation = opts.completion.documentation or {}
+  opts.completion.documentation.auto_show = true
 
-	opts.snippet = opts.snippet or {}
-	opts.snippet.expand = function(args)
-		require("luasnip").lsp_expand(args.body)
-	end
+  opts.fuzzy = opts.fuzzy or {}
+  opts.fuzzy.implementation = "prefer_rust"
 
-	opts.sources = opts.sources or {}
-	table.insert(opts.sources, { name = "nvim_lsp" })
-	table.insert(opts.sources, { name = "buffer" })
-	table.insert(opts.sources, { name = "path" })
-	table.insert(opts.sources, { name = "luasnip" })
+  opts.keymap = opts.keymap or {}
+  opts.keymap.preset = "none"
 
-	opts.mapping = opts.mapping or {}
-	opts.mapping["<CR>"] = cmp.mapping.confirm({
-		behavior = cmp.ConfirmBehavior.Insert,
-		select = true,
-	})
-	opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_next_item()
-		elseif require("luasnip").expand_or_jumpable() then
-			require("luasnip").expand_or_jump()
-		else
-			fallback()
-		end
-	end, { "i", "s" })
-	opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item()
-		elseif require("luasnip").jumpable(-1) then
-			require("luasnip").jump(-1)
-		else
-			fallback()
-		end
-	end, { "i", "s" })
+  opts.keymap["<CR>"] = { "accept" }
+  opts.keymap["<Tab>"] = { "select_next" }
+  opts.keymap["<S-Tab>"] = { "select_prev" }
 
-	return opts
+  return opts
 end
 
 return M
