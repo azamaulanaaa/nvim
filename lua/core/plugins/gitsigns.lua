e3c06b08c4ac1ec76ca89364eb21d87c8b8f1002
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-01 19:54:46 +0700

    chore(core): reformat gitsigns

--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -1,9 +1,9 @@
 local M = { "lewis6991/gitsigns.nvim" }
 
 M.opts = function(_, opts)
-	opts.current_line_blame = true
+  opts.current_line_blame = true
 
-	return opts
+  return opts
 end
 
 return M
