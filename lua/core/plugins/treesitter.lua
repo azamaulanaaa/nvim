8bad54420305a8b88b06a6c86e733fac3dd1682d
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-01 19:53:31 +0700

    chore(core): reformat treesitter

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,39 +1,39 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
 }
 
 M.event = function(_, events)
-	table.insert(events, "BufReadPost")
-	table.insert(events, "BufNewFile")
+  table.insert(events, "BufReadPost")
+  table.insert(events, "BufNewFile")
 
-	return events
+  return events
 end
 
 M.cmd = function(_, cmds)
-	table.insert(cmds, "TSInstall")
-	table.insert(cmds, "TSBufEnable")
-	table.insert(cmds, "TSBufDisable")
-	table.insert(cmds, "TSModuleInfo")
+  table.insert(cmds, "TSInstall")
+  table.insert(cmds, "TSBufEnable")
+  table.insert(cmds, "TSBufDisable")
+  table.insert(cmds, "TSModuleInfo")
 
-	return cmds
+  return cmds
 end
 
 M.opts = function(_, opts)
-	opts.ensure_installed = { "lua", "vim", "vimdoc" }
-	opts.use_languagetree = true
-	opts.auto_install = true
+  opts.ensure_installed = { "lua", "vim", "vimdoc" }
+  opts.use_languagetree = true
+  opts.auto_install = true
 
-	opts.highlight = opts.highlight or {}
-	opts.highlight.enable = true
+  opts.highlight = opts.highlight or {}
+  opts.highlight.enable = true
 
-	opts.indent = opts.indent or {}
-	opts.indent.enable = true
+  opts.indent = opts.indent or {}
+  opts.indent.enable = true
 
-	opts.incremental_selection = opts.incremental_selection or {}
-	opts.incremental_selection.enable = true
+  opts.incremental_selection = opts.incremental_selection or {}
+  opts.incremental_selection.enable = true
 
-	return opts
+  return opts
 end
 
 return M
