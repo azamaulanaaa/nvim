743822a0b6e606cfdc35f906a6d8be51b2e97bf1
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-02 16:18:41 +0700

    feat(custom): add lua lsp and formatter

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,6 +2,7 @@ local M = { "stevearc/conform.nvim" }
 
 M.opts = function(_, opts)
   opts.formatters_by_ft = opts.formatters_by_ft or {}
+  opts.formatters_by_ft.lua = { "stylua" }
   opts.formatters_by_ft.rust = { "rustfmt" }
 
   return opts
