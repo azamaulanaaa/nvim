1ac98e6b9818ca63cf3eb0061cd65936f64cc16a
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-02 08:29:06 +0700

    chore(core: reformat mason lspconfig

--- lua/core/plugins/mason-lspconfig.lua
+++ lua/core/plugins/mason-lspconfig.lua
@@ -1,13 +1,13 @@
 local M = {
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = {
-    "mason-org/mason.nvim",
-    "neovim/nvim-lspconfig",
-  },
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
 }
 
 M.opts = function(_, opts)
-  return opts
+	return opts
 end
 
 return M
