1e106348e72f2cd7f9f7f5c406f33feb9191e1bf
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-03 08:51:07 +0700

    chore(custom): reformat

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,11 +1,11 @@
 local M = { "stevearc/conform.nvim" }
 
 M.opts = function(_, opts)
-  opts.formatters_by_ft = opts.formatters_by_ft or {}
-  opts.formatters_by_ft.lua = { "stylua" }
-  opts.formatters_by_ft.rust = { "rustfmt" }
+	opts.formatters_by_ft = opts.formatters_by_ft or {}
+	opts.formatters_by_ft.lua = { "stylua" }
+	opts.formatters_by_ft.rust = { "rustfmt" }
 
-  return opts
+	return opts
 end
 
 return M
