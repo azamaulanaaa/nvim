a788dd38ed91a0c2b1990157557021aaed666d8f
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-03 08:51:27 +0700

    feat(custom): add deno format and lsp

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -4,6 +4,10 @@ M.opts = function(_, opts)
 	opts.formatters_by_ft = opts.formatters_by_ft or {}
 	opts.formatters_by_ft.lua = { "stylua" }
 	opts.formatters_by_ft.rust = { "rustfmt" }
+	opts.formatters_by_ft.javascript = { "deno_fmt" }
+	opts.formatters_by_ft.typescript = { "deno_fmt" }
+	opts.formatters_by_ft.javascriptreact = { "deno_fmt" }
+	opts.formatters_by_ft.typescriptreact = { "deno_fmt" }
 
 	return opts
 end
