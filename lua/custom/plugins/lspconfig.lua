ca3cd7fe1928c44a9b7c1a7e4b564acca73c6e7f
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-08 16:31:58 +0700

    feat(custom): add json lsp and formatter

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -7,6 +7,8 @@ M.opts = function(_, opts)
 	vim.lsp.enable("rust_analyzer")
 	-- javascript, typescript, javascriptreact, typescriptreact
 	vim.lsp.enable("denols")
+	-- json
+	vim.lsp.enable("jsonls")
 
 	return opts
 end
