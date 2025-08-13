d49157f559c81faaee58256eb8b895ed2ade04ad
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-13 08:14:58 +0700

    fix(core): use versioned gitsigns

--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -1,16 +1,19 @@
-local M = { "lewis6991/gitsigns.nvim" }
+local M = {
+	"lewis6991/gitsigns.nvim",
+	version = "*",
+}
 
 M.event = function(_, events)
-  table.insert(events, "BufReadPost")
-  table.insert(events, "BufNewFile")
+	table.insert(events, "BufReadPost")
+	table.insert(events, "BufNewFile")
 
-  return events
+	return events
 end
 
 M.opts = function(_, opts)
-  opts.current_line_blame = true
+	opts.current_line_blame = true
 
-  return opts
+	return opts
 end
 
 return M
