1cd8d08f1a37ccf145babc0d500d0d74359839a7
Aza Maulana <azamaulanaaa@gmail.com>
2025-08-30 13:45:35 +0700

    feat(custom): predefined enabled module of colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,6 +4,23 @@ local M = {
 }
 
 M.opts = function(_, opts)
+	opts.options = opts.options or {}
+	opts.options.module_default = false
+
+	opts.options.modules = opts.options.modules or {}
+	opts.options.modules.cmp = true
+	opts.options.modules.diagnostic = true
+	opts.options.modules.gitsigns = true
+	opts.options.modules["indent_blanklines"] = true
+	opts.options.modules["lazy.nvim"] = true
+	opts.options.modules.leap = true
+	opts.options.modules.native_lsp = true
+	opts.options.modules.nvimtree = true
+	opts.options.modules.treesitter = true
+	opts.options.modules.whichkey = true
+end
+
+M.opts = function(_, opts)
 	return opts
 end
 
