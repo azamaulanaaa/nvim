f0862a2b496be9b48d72f5e0b2f150a39f917e7b
Aza Maulana <azamaulanaaa@gmail.com>
2026-04-24 11:16:25 +0700

    feat(custom): outline auto close

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -12,4 +12,16 @@ M.opts = function(_, opts)
   opts.outline_window.focus_on_open = false
 end
 
+M.config = function(_, opts)
+  require("outline").setup(opts)
+
+  vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+      if vim.bo.filetype == "Outline" and vim.fn.winnr('$') == 1 then
+        vim.cmd("q")
+      end
+    end,
+  })
+end
+
 return M
