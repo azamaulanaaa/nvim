8c3dd4044ea307ac52d5847e855686e22e637d4d
Aza Maulana <azamaulanaaa@gmail.com>
2026-06-06 12:03:14 +0700

    feat(custom): conform add prettier and prettierd

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,20 +2,23 @@ local M = { "stevearc/conform.nvim" }
 
 M.opts = function(_, opts)
 	opts.formatters_by_ft = opts.formatters_by_ft or {}
+
 	opts.formatters_by_ft.lua = { "stylua" }
 	opts.formatters_by_ft.rust = { "rustfmt" }
-	opts.formatters_by_ft.javascript = { "deno_fmt" }
-	opts.formatters_by_ft.typescript = { "deno_fmt" }
-	opts.formatters_by_ft.javascriptreact = { "deno_fmt" }
-	opts.formatters_by_ft.typescriptreact = { "deno_fmt" }
-	opts.formatters_by_ft.json = { "deno_fmt" }
-	opts.formatters_by_ft.markdown = { "deno_fmt" }
-	opts.formatters_by_ft.css = { "deno_fmt" }
-	opts.formatters_by_ft.html = { "deno_fmt" }
 	opts.formatters_by_ft.toml = { "taplo" }
 	opts.formatters_by_ft.python = { "ruff_format" }
-	opts.formatters_by_ft.yaml = { "deno_fmt" }
 	opts.formatters_by_ft.proto = { "buf" }
+
+	local web_fallback = { "deno_fmt", "prettierd", "prettier", stop_after_first = true }
+	opts.formatters_by_ft.javascript = web_fallback
+	opts.formatters_by_ft.typescript = web_fallback
+	opts.formatters_by_ft.javascriptreact = web_fallback
+	opts.formatters_by_ft.typescriptreact = web_fallback
+	opts.formatters_by_ft.json = web_fallback
+	opts.formatters_by_ft.markdown = web_fallback
+	opts.formatters_by_ft.css = web_fallback
+	opts.formatters_by_ft.html = web_fallback
+	opts.formatters_by_ft.yaml = web_fallback
 
 	return opts
 end
